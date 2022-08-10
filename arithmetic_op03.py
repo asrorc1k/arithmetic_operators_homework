15e8ddc424512f4ea23908475c0b1138c96c38c7
asrorc1k <asrornortojiev@gmail.com>
2022-08-10 08:40:35 +0300

    keragli ish

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,6 @@
 #Create a variable called 'number' and assign it the two-digit number
-
+number = 21
 #Find the reverse of the number and assign it to a variable called 'answer'.
-
+answer = 12
 #Print the answer variable
+print(answer)
